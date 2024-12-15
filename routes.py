300528fb4f45842833ad140111e86989d9f8dc59
Quadr1on <153852090+Quadr1on@users.noreply.github.com>
2024-12-15 17:01:28 +0530

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, render_template, jsonify, request
 from models import db, City, Experience
 
-main = Blueprint('main', __name__)
+app = Blueprint('app', __name__)
 
-@main.route('/')
+@app.route('/')
 def index():
     cities = City.query.all()
     return render_template('index.html', cities=cities)
 
-@main.route('/api/experiences/<city_id>')
+@app.route('/api/experiences/<city_id>')
 def get_experiences(city_id):
     experiences = Experience.query.filter_by(city_id=city_id, active=True).all()
     
@@ -38,7 +38,7 @@ def get_experiences(city_id):
     
     return jsonify(organized_experiences)
 
-@main.route('/api/search')
+@app.route('/api/search')
 def search_experiences():
     query = request.args.get('q', '').lower()
     city_id = request.args.get('city_id')
@@ -57,10 +57,10 @@ def search_experiences():
     
     return jsonify(results)
 
-@main.route('/about')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@main.route('/contact')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
